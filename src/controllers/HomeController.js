32adfc2a87d1e4fb6256ff196fe1f3174ba26952
SinhDuc <terragm01@gmail.com>
2021-10-16 23:29:17 -1100

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import request from "request";
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 let getHomePage = (req, res) => {
   return res.render('homepage.ejs');
@@ -144,7 +145,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
@@ -156,9 +157,35 @@ function callSendAPI(sender_psid, response) {
   });
 }
 
+let setupProfile = async (req, res) => {
+  //call api facebook profile
+  // Construct the message body
+  let request_body = {
+    "get_started": { "payload": "GET_STARTED" },
+    "whitelisted_domains": ["https://chatbotbysd.herokuapp.com/"]
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  await request({
+    "uri": `https://graph.facebook.com/v12.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`,
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    console.log(body);
+    if (!err) {
+      console.log('Setup succeeds!')
+    } else {
+      console.error("Unable setup!" + err);
+    }
+  });
+  return res.send("Setup succeeds!");
+
+}
 module.exports = {
   getHomePage: getHomePage,
   postWebhook: postWebhook,
-  getWebhook: getWebhook
+  getWebhook: getWebhook,
+  setupProfile: setupProfile,
 }
 
